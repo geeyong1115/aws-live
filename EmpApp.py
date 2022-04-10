6d4fe6aabbd885971f3f7b7636c289684e12482d
geeyong1115 <53674303+geeyong1115@users.noreply.github.com>
2022-04-10 19:37:52 +0800

    asd

--- EmpApp.py
+++ EmpApp.py
@@ -113,7 +113,7 @@ def updateEmp(empid):
     return render_template('Edit.html', results=results)
 
 
-@app.route("/triggerUpdate", methods=['GET', 'POST'])
+@app.route("/triggerUpdate", methods=['POST'])
 def triggerUpdate():
 
     empid = request.form['empid']
