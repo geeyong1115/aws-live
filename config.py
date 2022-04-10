f65a63ae14b9b386c4b65aa4f460276aec040d57
Tay Gee Yong <53674303+geeyong1115@users.noreply.github.com>
2022-04-10 11:11:04 +0800

    Add files via upload

---
+++ config.py
@@ -0,0 +1,6 @@
+customhost = "employee.cueoqeh1xxtj.us-east-1.rds.amazonaws.com"
+customuser = "geeyong"
+custompass = "Taygeeyong1214"
+customdb = "employee"
+custombucket = "taygeeyong-employee"
+customregion = "us-east-1"
